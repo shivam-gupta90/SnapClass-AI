bf61717d66b268f3d8ec3d800d1c065f9cf7b26c
Shivam Gupta <shivdevil480@gmail.com>
2026-07-27 13:46:37 +0530

    QR code , making dialog for teacher

--- src/ui/base_layout.py
+++ src/ui/base_layout.py
@@ -77,7 +77,7 @@ def style_base_layout():
             }
             button[kind="primary"]{
                 border-radius:1.5rem !important;
-                background: #5865F2 !important;
+                background-color: #5865F2 !important;
                 color : white !important;
                 padding :10px 20px !important;
                 border: none !important;
@@ -87,7 +87,7 @@ def style_base_layout():
             
             button[kind="secondary"]{
                 border-radius:1.5rem !important;
-                background: #EB459E !important;
+                background-color: #EB459E !important;
                 color : white !important;
                 padding :10px 20px !important;
                 border: none !important;
@@ -96,7 +96,7 @@ def style_base_layout():
                 
             button[kind="tertiary"]{
                 border-radius:1.5rem !important;
-                background: #EB459E !important;
+                background-color: black !important;
                 color : white !important;
                 padding :10px 20px !important;
                 border: none !important;
